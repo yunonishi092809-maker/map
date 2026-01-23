1cbb0884be8699c09317e09ba37a30d355a55963
Yuno <yunonishi092809@gmail.com>
2026-01-23 22:52:59 +0900

    Initial Commit

---
+++ map/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  map
+//
+//  Created by 西村 on 2026/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ map/mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapApp.swift
+//  map
+//
+//  Created by 西村 on 2026/01/23.
+//
+
+import SwiftUI
+
+@main
+struct mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
